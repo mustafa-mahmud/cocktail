968d2401ea835586432c79f47532b5826412dcf4
Mithu Khan <mithuweb000@gmail.com>
2022-08-20 13:11:13 +0600

    init project

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-">About Us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nisi, nam quia soluta, magni placeat excepturi itaque inventore dolorum
+        architecto nemo atque corporis a molestiae? Voluptas error eos esse
+        dolor debitis dolore quidem eius provident, ut quis illum, distinctio,
+        amet libero totam modi? Eaque obcaecati, quibusdam ab aliquid possimus
+        minima.
+      </p>
+    </section>
+  );
+};
+
+export default About;
